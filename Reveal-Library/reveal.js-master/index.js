61a1d50c4017bede5311ba9eb573a1351acfb6d9
Lana <sekerija.lana@gmail.com>
2022-03-15 14:26:26 +0100

    es wurden 2 Ordner erstellt für das alte und neue Projekt, und die Library eingefügt

---
+++ Reveal-Library/reveal.js-master/index.js
@@ -0,0 +1,7 @@
+Reveal.initialize({
+    hash: true,
+    controls: true,
+    
+    // Learn about plugins: https://revealjs.com/plugins/
+    plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
+});
